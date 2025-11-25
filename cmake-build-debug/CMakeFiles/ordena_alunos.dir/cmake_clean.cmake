5dc093527d52e9665ba113764f1331f79aa2e9c8
allysson-assuncao <anybody723@proton.me>
2025-11-25 07:03:11 -0300

    reorganizando os arquivos da maratona e adicionando os da atual para prática em 2025

---
+++ cmake-build-debug/CMakeFiles/ordena_alunos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ordena_alunos.dir/link.d"
+  "CMakeFiles/ordena_alunos.dir/maratonaIF2025/estudosC++/ordena_alunos.cpp.o"
+  "CMakeFiles/ordena_alunos.dir/maratonaIF2025/estudosC++/ordena_alunos.cpp.o.d"
+  "ordena_alunos"
+  "ordena_alunos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ordena_alunos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
